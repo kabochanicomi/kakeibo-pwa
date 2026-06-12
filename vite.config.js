8d32104424287b1834b92e13fc83643dd642f4bf
work <>
2026-06-13 08:13:19 +0900

    UI改善

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false, // public/manifest.json を使用
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,ico,svg}'],
+        navigateFallback: '/kakeibo-pwa/index.html',
+      },
+    }),
+  ],
   base: '/kakeibo-pwa/',
 })
